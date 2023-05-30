223e2feb718830c4191e459d5642d050310371b9
mangos <127545605+GleyberDay25@users.noreply.github.com>
2023-05-29 21:40:45 -0400

    Delete say.js

--- say.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = (client, message, args) => {
-  let toSay = args.join(" ")
-  if(!toSay) return message.channel.send({content: "You have to provide something!"})
-  message.channel.send({content: toSay})
-}
-
-module.exports.name = "say"
